d722ee99b73a7756b09a865ff4a42de6d471de7b
Matias Rolón <matiasrolon1301@gmail.com>
2023-11-29 10:17:34 -0300

    create heroSection

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,20 @@ import { darkTheme, lightTheme } from './utils/Themes.js'
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar/index.js';
 import { useState } from 'react';
+import styled from "styled-components";
+import HeroSection from './components/HeroSection/index';
 
 
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`
+const Wrapper = styled.div`
+  background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
+`
 
 function App() {
 
@@ -17,6 +29,9 @@ function App() {
       <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <Router >
         <Navbar />
+        <Body>
+          <HeroSection/>
+        </Body>
       </Router>
       </ThemeProvider>
     </>
--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,5 +1,5 @@
 export const Bio = {
-    name: "Rishav Chanda",
+    name: "Matias Rolon",
     roles: [
       "Full Stack Developer",
       "Android Developer",
